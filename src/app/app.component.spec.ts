a5a0c8762144aab18ee13edf7971ac235b267221
Design Updates <>
2024-04-11 10:16:16 +0200

    Renamed Project, changed Text

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Freitextanalyse-New' title`, () => {
+  it(`should have the 'Freitextanalyse' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Freitextanalyse-New');
+    expect(app.title).toEqual('Freitextanalyse');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Freitextanalyse-New');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Freitextanalyse');
   });
 });
